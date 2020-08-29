7c97c7b6b474c77fa3239be1773369adfece0aa3
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2020-08-29 19:34:10 -0400

    Adding a third tab for settings

---
+++ NHL Tracker/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  NHL Tracker
+//
+//  Created by Anthony Hopkins on 2020-08-29.
+//  Copyright © 2020 Anthony Hopkins. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
