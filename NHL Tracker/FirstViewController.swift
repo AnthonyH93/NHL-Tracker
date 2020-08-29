44a499405049b726191b6e8a11a2cab34ec36159
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2020-08-29 19:18:36 -0400

    Initial Commit

---
+++ NHL Tracker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NHL Tracker
+//
+//  Created by Anthony Hopkins on 2020-08-29.
+//  Copyright © 2020 Anthony Hopkins. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NHL Tracker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NHL Tracker
+//
+//  Created by Anthony Hopkins on 2020-08-29.
+//  Copyright © 2020 Anthony Hopkins. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
