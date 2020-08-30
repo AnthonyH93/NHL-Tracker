d8fa40ee0895cb16dcb394c74144d4e865375828
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2020-08-29 20:05:31 -0400

    Setting up the favourite team picker

---
+++ NHL Tracker/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  NHL Tracker
+//
+//  Created by Anthony Hopkins on 2020-08-29.
+//  Copyright © 2020 Anthony Hopkins. All rights reserved.
+//
+
+import Foundation
+    
+//Picker Favourite Teams
+let NHLTeamsStringArray: [String] = ["Team 1", "Team 2", "Team 3", "Team 4"]
--- NHL Tracker/SettingsViewController.swift
+++ NHL Tracker/SettingsViewController.swift
@@ -8,23 +8,39 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    //MARK: Properties
+    @IBOutlet weak var favouriteTeamPicker: UIPickerView!
+    
+    //MARK: Variables
+    var pickerData: [String] = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Connect data:
+        self.favouriteTeamPicker.delegate = self
+        self.favouriteTeamPicker.dataSource = self
+        
+        pickerData = NHLTeamsStringArray
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: Picker Functions
+    //Number of columns of data for the favourite team picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    
+    //Number of rows of data for the favourite team picker
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return fopr the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
 }
