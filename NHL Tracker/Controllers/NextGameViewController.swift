fd07bf1d685f3707d23509bb911b016ebdafada1
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2020-09-04 00:18:16 -0400

    Adding live score functionality to next game view

--- NHL Tracker/Controllers/NextGameViewController.swift
+++ NHL Tracker/Controllers/NextGameViewController.swift
@@ -71,6 +71,13 @@ class NextGameViewController: UIViewController {
         var nextGameAvailable = false
         var prevGameAvailable = false
         
+        var nextGamePK = 0
+        var prevGamePK = 0
+        
+        var homeTeamScore = 0
+        var awayTeamScore = 0
+        var currentPeriod = ""
+        
         //Decide label initial values based on saved favourite team
         if let savedFavouriteTeam = dataPersistence.loadFavouriteNHLTeam() {
             let favouriteTeamName = savedFavouriteTeam.favouriteTeam
@@ -97,6 +104,7 @@ class NextGameViewController: UIViewController {
                         nextGameTeam1 = self.teamConversions.teamNameToShortName(teamToConvert: favouriteTeamName)
                         nextGameVenue = "@" + nextGame.dates[0].games[0].venue.name
                         nextGameStatus = nextGame.dates[0].games[0].status.detailedState
+                        nextGamePK = nextGame.dates[0].games[0].gamePk
                         
                         //Format date to display on the screen
                         if let date = self.dateFormatterGet.date(from: nextGame.dates[0].games[0].gameDate) {
@@ -130,6 +138,7 @@ class NextGameViewController: UIViewController {
                         prevGameTeam1 = self.teamConversions.teamNameToShortName(teamToConvert: favouriteTeamName)
                         prevGameVenue = "@" + previousGame.dates[0].games[0].venue.name
                         prevGameStatus = previousGame.dates[0].games[0].status.detailedState
+                        prevGamePK = previousGame.dates[0].games[0].gamePk
                         
                         //Format date to display on the screen
                         if let date = self.dateFormatterGet.date(from: previousGame.dates[0].games[0].gameDate) {
@@ -151,13 +160,55 @@ class NextGameViewController: UIViewController {
                 group.wait()
             }
             
-            //Operation to update UI after API calls are complete
+            
+            //Operation to get the linescore of a game with the given PK found in either next game call or previous game call
             let operation2 = BlockOperation {
+                let group = DispatchGroup()
+                if (nextGameAvailable) {
+                    group.enter()
+                    //Use the next game if it was found, score might not exist if the game hasn't started
+                    self.nhlApiServices.fetchLineScore(teamPK: nextGamePK) { responseObject, error in
+                        guard let responseObject = responseObject, error == nil else {
+                            print(error ?? "Unknown error with previous game")
+                            return
+                        }
+                        print(responseObject)
+                        //Use the response object and extract data needed
+                        homeTeamScore = responseObject.teams.home.goals
+                        awayTeamScore = responseObject.teams.away.goals
+                        currentPeriod = responseObject.currentPeriodOrdinal
+                        group.leave()
+                    }
+                    group.wait()
+                }
+                else {
+                    if (prevGameAvailable) {
+                        group.enter()
+                        //Use the previous game if the next game was not found and the previous game was found
+                        self.nhlApiServices.fetchLineScore(teamPK: prevGamePK) { responseObject, error in
+                            guard let responseObject = responseObject, error == nil else {
+                                print(error ?? "Unknown error with previous game")
+                                return
+                            }
+                            print(responseObject)
+                            //Use the response object and extract data needed
+                            homeTeamScore = responseObject.teams.home.goals
+                            awayTeamScore = responseObject.teams.away.goals
+                            currentPeriod = responseObject.currentPeriodOrdinal
+                            group.leave()
+                        }
+                        group.wait()
+                    }
+                }
+            }
+            //Operation to update UI after API calls are complete
+            let operation3 = BlockOperation {
                 //Update UI based on the result of the API calls
                 DispatchQueue.main.async {
                     //Prioritize next game if it is available
                     if (nextGameAvailable){
                         self.previousGameLabel.isHidden = true
+                        self.periodLabel.isHidden = false
                         self.team1Label.text = nextGameTeam1
                         self.team2label.text = nextGameTeam2
                         self.record1Label.text = nextGameRecord1
@@ -165,20 +216,26 @@ class NextGameViewController: UIViewController {
                         self.timeLabel.text = nextGameDate
                         self.cityLabel.text = nextGameVenue
                         self.gameStateLabel.text = self.gameHelper.fixStatusCode(statusCode: nextGameStatus)
+                        self.periodLabel.text = currentPeriod
                         
                         //Bold the home team
                         if (nextGameIsHome) {
                             self.team1Label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
                             self.team2label.font = UIFont.systemFont(ofSize: 22, weight: .regular)
+                            self.score1Label.text = String(homeTeamScore)
+                            self.score2Label.text = String(awayTeamScore)
                         }
                         else {
                             self.team1Label.font = UIFont.systemFont(ofSize: 22, weight: .regular)
                             self.team2label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+                            self.score1Label.text = String(awayTeamScore)
+                            self.score2Label.text = String(homeTeamScore)
                         }
                     }
                     else {
                         if (prevGameAvailable){
                             self.previousGameLabel.isHidden = false
+                            self.periodLabel.isHidden = true
                             self.team1Label.text = prevGameTeam1
                             self.team2label.text = prevGameTeam2
                             self.record1Label.text = prevGameRecord1
@@ -191,10 +248,14 @@ class NextGameViewController: UIViewController {
                             if (prevGameIsHome) {
                                 self.team1Label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
                                 self.team2label.font = UIFont.systemFont(ofSize: 22, weight: .regular)
+                                self.score1Label.text = "\(homeTeamScore)"
+                                self.score2Label.text = "\(awayTeamScore)"
                             }
                             else {
                                 self.team1Label.font = UIFont.systemFont(ofSize: 22, weight: .regular)
                                 self.team2label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+                                self.score1Label.text = "\(awayTeamScore)"
+                                self.score2Label.text = "\(homeTeamScore)"
                             }
                         }
                     }
@@ -203,10 +264,12 @@ class NextGameViewController: UIViewController {
             
             //Block the UI updates until all API calls are finished
             operation2.addDependency(operation1)
+            operation3.addDependency(operation2)
             
             //Add operations to the operation queue
             queue.addOperation(operation1)
             queue.addOperation(operation2)
+            queue.addOperation(operation3)
             
         }
         else {
--- NHL Tracker/Model/NHLApiServices.swift
+++ NHL Tracker/Model/NHLApiServices.swift
@@ -10,6 +10,7 @@ import Foundation
 
 struct NHLApiServices {
     
+    //API call to get the next game for a specified teamID
     func fetchNextGame(teamID: Int64, completion: @escaping (OuterTeam_Next?, Error?) -> Void) {
         let params = ["expand":"team.schedule.next"]
         let url = "https://statsapi.web.nhl.com/api/v1/teams/" + String(teamID)
@@ -40,6 +41,7 @@ struct NHLApiServices {
         task.resume()
     }
     
+    //API call to get the previous game for a specified teamID
     func fetchPreviousGame(teamID: Int64, completion: @escaping (OuterTeam_Previous?, Error?) -> Void) {
         let params = ["expand":"team.schedule.previous"]
         let url = "https://statsapi.web.nhl.com/api/v1/teams/" + String(teamID)
@@ -70,4 +72,27 @@ struct NHLApiServices {
         task.resume()
     }
     
+    //API call to get the linescore for a game with the given teamPD
+    func fetchLineScore(teamPK: Int, completion: @escaping (LineScore?, Error?) -> Void) {
+        let urlString = "https://statsapi.web.nhl.com/api/v1/game/" + String(teamPK) + "/linescore"
+        let url = URL(string: urlString)
+        let request = URLRequest(url: url!)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            //Make sure there is data
+            guard let data = data,
+                let response = response as? HTTPURLResponse,
+                //Make sure the status code is OK
+                (200 ..< 300) ~= response.statusCode,
+                //Make sure there is no error
+                error == nil else {
+                    completion(nil, error)
+                    return
+            }
+
+            let responseObject = (try? JSONDecoder().decode(LineScore.self, from: data))
+            completion(responseObject, nil)
+        }
+        task.resume()
+    }
 }
